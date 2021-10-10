ecb559f04c7a221baa4ff9a19c1b6bfd6146bd4a
RaghavMangrola <dev@raghav.io>
2021-10-10 10:00:23 -0700

    Connected wallet to web app

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,34 +1,71 @@
 import type { NextPage } from 'next'
-import React, {useEffect} from 'react'
-import {ethers} from 'ethers'
+import React, { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
 
 const Home: NextPage = () => {
-    const checkIfWalletIsConnected = () => {
-        const { ethereum } = window
+  const [currentAccount, setCurrentAccount] = useState('')
 
-        if (!ethereum) {
-            console.log('Make sure you have metamask!')
-            return
-        } else {
-            console.log('We have the ethereum object ', ethereum)
-        }
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window as any
+
+      if (!ethereum) {
+        console.log('Make sure you have metamask!')
+        return
+      } else {
+        console.log('We have the ethereum object ', ethereum)
+      }
+
+      const accounts = await ethereum.request({ method: 'eth_accounts' })
+
+      if (accounts.length !== 0) {
+        const account = accounts[0]
+        console.log('Found an authorized account:', account)
+        setCurrentAccount(account)
+      } else {
+        console.log('No authorized account found')
+      }
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    useEffect(() => {
-        checkIfWalletIsConnected()
-    }, [])
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window as any
 
-    return (
-        <div className='container mx-auto'>
-            <div className='container mx-auto'>
-                👋🏽 Hey There!
-                 I am raghav and I work on event technology! Connect your Ethereum wallent and wave at me!
-                <button>
-                    Wave at Me
-                </button>
-            </div>
-        </div>
-    )
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]); 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+  }, [])
+
+  return (
+    <div className="container mx-auto">
+      👋🏽 Hey There! I am raghav and I work on event technology! Connect your
+      Ethereum wallent and wave at me!
+      <button>Wave at Me</button>
+
+      {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+    </div>
+  )
 }
 
 export default Home
